344424a02baa375e15583deca5f13de68ab290a8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-14 09:14:13 +0530

    new commit

---
+++ App.js
@@ -0,0 +1,28 @@
+const heading = React.createElement(
+    'h1',
+    {
+        id: 'title',
+    },
+    'Heading 1'
+);
+const heading2 = React.createElement(
+    'h1',
+    {
+        id: 'title',
+    },
+    'Heading 2'
+);
+
+const container = React.createElement(
+    'div',
+    {
+        id: 'container',
+    },
+    [heading1, heading2]
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//Passing the react element inside the root
+//asyc and defer difference is HW
+root.render(container);
