cd6e66bb363ebdd39cd5f883d555824e2441fc1c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-14 14:21:50 +0530

    new stuff

--- jsrefresor.js
+++ jsrefresor.js
@@ -5,7 +5,6 @@ function normalFn(params) {
 
 
     if (a >= 10) {
-
         console.log(`normal function`)
     } else {
         console.log(`a is less than  10 and that is ${a}`)
@@ -15,7 +14,6 @@ function normalFn(params) {
 const arrowFn = () => {
 
     if (a >= 10) {
-
         console.log(`Arrow Function`)
     } else {
         console.log(`a is less than  10 and that is ${a}`)
