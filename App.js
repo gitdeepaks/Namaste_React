c4461c11a16508b180594a5d55dcf63e454445d6
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-22 19:39:01 +0530

    new

--- App.js
+++ App.js
@@ -17,14 +17,16 @@
  * 
  * **/
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent =
     React.createElement('div',
         { id: 'parent' },
         [React.createElement('div', { id: 'child' },
             [
-                React.createElement('h1', {}, "I am an h1 tag"),
-                React.createElement('h2', {}, "I am an h2 tag")
+                React.createElement('h1', {}, "This is Namaste React 💎"),
+                React.createElement('h2', {}, "By Deepak Sankhyan 🧑🏻‍💻")
             ]),
         React.createElement('div', { id: 'child2' },
             [
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 💎
+
+## Parcel
+
+- Dev
+- Local Server
+- Automatic Refresh i.e. HMR = Hot Module Replacement
+- File Watching Algorithem i.e. written in C++.
+- Faster Builds because if cashing.
+- Image Optimization
+- Minification of file
+- Bundling
+- Compress the file
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Daignostics
+- Error Handling
+- HTTPS Server Support
+- Tree Shaking - Remove Unused code for you.
+-
