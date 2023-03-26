d33eb88f146f0c64d08adb1afdc476d20d755867
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-26 20:41:38 +0530

    new

---
+++ Main.js
@@ -0,0 +1,41 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const elem = <span id="head2">React Element</span>
+
+const title = (
+     <h1 id="head"
+          className="head"
+          tabIndex="1"
+     >{elem}
+          Namaste React Using element JSX 🚀</h1>
+);
+const Title = () => (
+     <h1 id="head"
+          className="head"
+          tabIndex="1"
+     >{elem}
+          Namaste React Using JSX 🚀</h1>
+);
+
+//React Components
+
+//Functional Components
+//Component Composition
+const HeadingComponenet = () => (
+
+     <div id="container">
+          {title}
+          <Title />
+          <h1 id="head" className="heading">Namaste React Functional Component 🚀</h1>
+     </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(head);
+root.render(<HeadingComponenet />);
+
+
+
