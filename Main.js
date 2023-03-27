7f563cac83041cfb3c7e79d82d6dad148aa83afd
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-27 16:18:41 +0530

    new

--- Main.js
+++ Main.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 
 const elem = <span id="head2">React Element</span>
 
+const firstTest = <p>Hello there this is Live React Session</p>
+
 const title = (
      <h1 id="head"
           className="head"
@@ -17,7 +19,9 @@ const Title = () => (
           tabIndex="1"
      >
           {elem}
+          {firstTest}
           Namaste React Using JSX 🚀</h1>
+
 );
 
 //React Components
