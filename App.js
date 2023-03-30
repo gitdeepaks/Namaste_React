ea29ab9eef353684ac17876d71bb1afe71ac5625
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-30 21:29:26 +0530

    new

--- App.js
+++ App.js
@@ -7,15 +7,17 @@ const Header = () => {
      return (
           <div className="header">
                <div>
-                    <img className="logo" src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png" />
-               </div>
-               <div className="nav-items">
-                    <ul>
-                         <li>Home</li>
-                         <li>About</li>
-                         <li>Contact Us</li>
-                         <li>Cart</li>
-                    </ul>
+                    <div>
+                         <img className="logo" src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png" />
+                    </div>
+                    <div className="nav-items">
+                         <ul>
+                              <li>Home</li>
+                              <li>About</li>
+                              <li>Contact Us</li>
+                              <li>Cart</li>
+                         </ul>
+                    </div>
                </div>
           </div>
      )
@@ -28,39 +30,146 @@ const styleCard = {
 }
 
 const RestaurantCart = (props) => {
-
+     const { resData } = props;
 
      return (
           <div className="res-cart" style={styleCard}>
-               <img className="res-logo"
-                    src="https://www.indianveggiedelight.com/wp-content/uploads/2020/04/veg-biryani-instant-pot.jpg" />
-               <h3>{props.resName}</h3>
-               <h4>{props.cuisine}</h4>
-               <h4>4.4 stars</h4>
-               <h4>38 Minutes</h4>
+               <img
+                    className="res-logo"
+                    src={
+                         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + resData.data.cloudinaryImageId}
+
+               />
+               <h3>{resData.data.name}</h3>
+               <h4>{resData.data.cuisines.join(", ")}</h4>
+               <h4 h4 > {resData.data.avgRating} stars</h4>
+               <h4>{resData.data.costForTwo / 100}</h4>
+               <h4>{resData.data.deliveryTime} Minutes</h4>
           </div>
      )
+}
+
+const resObj = {
+     "type": "restaurant",
+     "data": {
+          "type": "F",
+          "id": "694226",
+          "name": "Garage bakery and cafe",
+          "uuid": "5e387feb-83d4-4b20-b6da-2df95e51e4ec",
+          "city": "12",
+          "area": "C Scheme",
+          "totalRatingsString": "Too Few Ratings",
+          "cloudinaryImageId": "44a4b325cd9f47e70905f55b5779b93c",
+          "cuisines": [
+               "Desserts",
+               "Snacks",
+               "Fast Food"
+          ],
+          "tags": [],
+          "costForTwo": 20000,
+          "costForTwoString": "₹200 FOR TWO",
+          "deliveryTime": 29,
+          "minDeliveryTime": 29,
+          "maxDeliveryTime": 29,
+          "slaString": "29 MINS",
+          "lastMileTravel": 1.399999976158142,
+          "slugs": {
+               "restaurant": "garage-bakery-and-cafe-c-scheme-c-scheme",
+               "city": "jaipur"
+          },
+          "cityState": "12",
+          "address": "J 17 D, Ramesh Marg, C Scheme, Ashok Nagar, Jaipur, Rajasthan 302002, India",
+          "locality": "Ashok Nagar",
+          "parentId": 416353,
+          "unserviceable": false,
+          "veg": false,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [],
+          "ribbon": [
+               {
+                    "type": "PROMOTED"
+               }
+          ],
+          "chain": [],
+          "feeDetails": {
+               "fees": [
+                    {
+                         "name": "distance",
+                         "fee": 2400,
+                         "message": ""
+                    },
+                    {
+                         "name": "time",
+                         "fee": 0,
+                         "message": ""
+                    },
+                    {
+                         "name": "special",
+                         "fee": 0,
+                         "message": ""
+                    }
+               ],
+               "totalFees": 2400,
+               "message": "",
+               "title": "Delivery Charge",
+               "amount": "2400",
+               "icon": ""
+          },
+          "availability": {
+               "opened": true,
+               "nextOpenMessage": "",
+               "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "cid=6282530~p=1~eid=00000187-3266-7ed6-074f-3c7d00110117",
+          "badges": {
+               "imageBased": [],
+               "textBased": [],
+               "textExtendedBadges": []
+          },
+          "lastMileTravelString": "1.3 kms",
+          "hasSurge": false,
+          "sla": {
+               "restaurantId": "694226",
+               "deliveryTime": 29,
+               "minDeliveryTime": 29,
+               "maxDeliveryTime": 29,
+               "lastMileTravel": 1.399999976158142,
+               "lastMileDistance": 0,
+               "serviceability": "SERVICEABLE",
+               "rainMode": "NONE",
+               "longDistance": "NOT_LONG_DISTANCE",
+               "preferentialService": false,
+               "iconType": "EMPTY"
+          },
+          "promoted": true,
+          "avgRating": "--",
+          "totalRatings": 0,
+          "new": true
+     },
+     "subtype": "basic"
 }
 
 const Body = () => {
 
      return (
           <div className="body">
-               <div>
-                    <div className="search">
-                         <input className="search" type={"search"} />
-                    </div>
-                    <div className="res-container">
-                         <RestaurantCart
-                              resName="Meghna Food"
-                              cuisine="Biryani, North Indian, Asian"
-                         />
-                         <RestaurantCart resName="KFC"
-                              cuisine="Burger, Fast Food"
-                         />
 
-                    </div>
+               <div className="search">
+                    Search
                </div>
+               <div className="res-container">
+                    <RestaurantCart
+                         resData={resObj}
+                    />
+
+
+               </div>
+
           </div>
      )
 }
