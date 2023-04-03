2c651f8bcf44c0ca385196ceaf9bb1b4a9275aee
gitdeepaks <deepaksankhyan92@outlook.com>
2023-04-03 16:50:14 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import RestaurantCart from "./components/RestaurantCart";
+import Body from "./components/Body";
+
+
+//not using keys (not acceptable) <<<<< index as keys <<<<<<< unique id (best practice)
+
+const AppLayout = () => {
+     return (
+          <div className="app">
+               <Header />
+               <Body />
+
+          </div>
+     )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(head);
+root.render(<AppLayout />);
+
+
+
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestaurantCart from './RestaurantCart';
 import resObj from '../utils/mockData';
 
 
 
+
 const Body = () => {
+    // Local State Variable - Super powerful variable
+    const [listOfRestaurants, setListOfRestraunt] = useState(resObj);
 
     return (
         <div className="body">
-
-            <div className="search">
-                Search
+            <div className="filter">
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.data.avgRating >= 4
+                        );
+                        setListOfRestraunt(filteredList);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
             </div>
             <div className="res-container">
-                {
-                    resObj.map((restaurant) =>
-                    (<RestaurantCart key={restaurant.data.id} resData={restaurant} />
-                    ))}
-
-
+                {listOfRestaurants.map((restaurant) => (
+                    <RestaurantCart key={restaurant.data.id} resData={restaurant} />
+                ))}
             </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default Body
